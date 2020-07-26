d12bde03bdea675b9a1f9b530ee8cc8a318c604e
[jerokun@gmail.com] <jerokun@gmail.com>
2020-07-26 18:33:32 +0200

    startbutton

--- src/app/lobby-page/lobby.page.ts
+++ src/app/lobby-page/lobby.page.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import * as _ from 'lodash';
 
+export interface NgStyle {
+  [klass: string]: any;
+}
+
 @Component({
   selector: 'app-lobby-page',
   templateUrl: './lobby-page.component.html',
@@ -10,11 +14,22 @@ export class LobbyPageComponent implements OnInit {
   constructor() {}
 
   game = false;
+  isButtonGroupClasses: true;
 
   ngOnInit() {}
 
   toggleGame() {
-    if (this.game === false) { this.game = true; }
-    else { this.game = false; }
+    if (this.game === false) {
+      this.game = true;
+    } else {
+      this.game = false;
+    }
+  }
+
+  getButtonGroupClasses() {
+    const buttonGroupClasses = {
+      buttongroupclasses: this.isButtonGroupClasses,
+    };
+    return buttonGroupClasses;
   }
 }
