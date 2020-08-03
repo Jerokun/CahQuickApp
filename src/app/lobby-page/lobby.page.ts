5c4f9bc8277f2cffbe86d0797a4299bb918153b9
Zweetkameel <markdewolf4@hotmail.com>
2020-08-03 18:38:05 +0200

    lobby page room code, socket io chatroom example

--- src/app/lobby-page/lobby.page.ts
+++ src/app/lobby-page/lobby.page.ts
@@ -22,3 +22,50 @@ export class LobbyPageComponent implements OnInit {
     }
   }
 }
+
+// import { Component, OnInit, OnChanges } from '@angular/core';
+// import * as _ from 'lodash';
+
+// @Component({
+//   selector: 'app-lobby-page',
+//   templateUrl: './lobby-page.component.html',
+//   styleUrls: ['./lobby-page.component.scss'],
+// })
+
+// export class LobbyPageComponent implements OnInit, OnChanges {
+//   constructor() {}
+
+//   game = false;
+//   isButtonGroupClasses: true;
+//   public roomCode: string[] = [];
+  
+
+//   ngOnChanges() {
+//     console.log('changes', this.roomCode);
+//   }
+
+//   ngOnInit() {
+//     function makeid(length) {
+//       var result           = '';
+//       var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+//       var charactersLength = characters.length;
+//       for ( var i = 0; i < length; i++ ) {
+//          result += characters.charAt(Math.floor(Math.random() * charactersLength));
+//       }
+//       return result;
+//     }
+//     this.roomCode = this.result();
+//     console.log('this.roomCode', this.roomCode);   
+
+//   }
+
+//   toggleGame() {
+//     if (this.game === false) {
+//       this.game = true;
+//     } else {
+//       this.game = false;
+//     }
+//   }
+// }
+
+
