ca7e4e65c7c598cce7f449dae81a2fc48e7375ca
[jerokun@gmail.com] <jerokun@gmail.com>
2020-09-06 23:50:07 +0200

    changes

---
+++ CahBackend/Models/Deck/WhiteCard.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CahBackend.Models.Deck
+{
+    public class WhiteCard
+    {
+        public string Text { get; set; }
+    }
+}
