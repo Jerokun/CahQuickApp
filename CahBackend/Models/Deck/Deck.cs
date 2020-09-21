1d1713d3b04c0dbd6f6d2da7ad21c3da0af4bf92
[jerokun@gmail.com] <jerokun@gmail.com>
2020-09-21 16:42:45 +0200

    commit be

--- CahBackend/Models/Deck/Deck.cs
+++ CahBackend/Models/Deck/Deck.cs
@@ -1,12 +1,14 @@
-﻿using System;
+﻿using CahBackend.Models.Deck;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace CahBackend.Models.Deck
 {
-    public class Deck
+    public class GameDeck
     {
-
+        public BlackCard[] BlackCards { get; set; }
+        public WhiteCard[] WhiteCards { get; set; }
     }
 }
---
+++ CahBackend/Models/Game.cs
@@ -0,0 +1,14 @@
+﻿using CahBackend.Models.Deck;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CahBackend.Models
+{
+    public class Game
+    {
+        public ApplicationUser[] Users { get; set; }
+        public GameDeck GameDeck { get;set; }
+    }
+}
