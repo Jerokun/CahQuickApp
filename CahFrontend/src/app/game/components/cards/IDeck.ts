45368136cd14f9fd9430370bdb3aba966095987c
[jerokun@gmail.com] <jerokun@gmail.com>
2020-10-11 17:03:20 +0200

    removed application

--- CahFrontend/src/app/game/components/cards/IDeck.ts
+++
@@ -1,6 +0,0 @@
-export interface IDeck {
-  name: string;
-  black: number[];
-  white: number[];
-  icon?: number | string;
-}
--- CahFrontend/src/app/game/components/cards/black-card/IBlackCard.ts
+++
@@ -1,4 +0,0 @@
-export interface IBlackCard {
-  text: string;
-  pick: number;
-}
