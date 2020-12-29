7742d20c34d653a89a946b345c054cb8072dd258
zweetkameel <markdewolf4@hotmail.com>
2020-12-29 22:58:06 +0100

    cleaned up lobbypage.ts moved to session.service

--- CahFrontend/src/app/lobby-page/new-session/new-session.component.ts
+++ CahFrontend/src/app/lobby-page/new-session/new-session.component.ts
@@ -8,15 +8,14 @@ import { ViewService } from 'src/app/services/lobby/view.service';
 	styleUrls: ['./new-session.component.scss'],
 })
 export class NewSessionComponent implements OnInit {
-
 	list = ['Deck 1', 'Deck 2', 'Deck 3'];
 	selected: string[];
 
 	constructor(public viewService: ViewService, public sessionService: SessionService) {}
 
 	ngOnInit(): void {
-    // this.sessionService.getDecklist().subscribe(t => this.list = t);
-  }
+		// this.sessionService.getDecklist().subscribe(t => this.list = t);
+	}
 
 	updateSelected(event: string[]): void {
 		console.log('newSession page has received an update from checkbox-list', event);
